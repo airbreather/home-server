e9a883a830408adfb5e6e18f720003d36d3214d2
Joe Amenta <airbreather@linux.com>
2024-12-28 12:06:38 -0500

    a few tweaks?

--- 01-services/samba/install.zsh
+++ 01-services/samba/install.zsh
@@ -3,7 +3,7 @@
 # too much insanity to containerize.
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-sudo cp $SCRIPT_DIR/smb.conf /etc/smb.conf
+sudo cp $SCRIPT_DIR/smb.conf /etc/samba/smb.conf
 sudo mkdir -p /var/lib/samba/usershares
 sudo chmod +t /var/lib/samba/usershares
 sudo zfs set sharesmb=on nas

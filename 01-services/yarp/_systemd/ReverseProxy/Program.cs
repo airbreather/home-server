019188a37ef86182c1f75abc3bedd5fc57569ee6
Joe Amenta <airbreather@linux.com>
2025-03-24 08:42:23 -0400

    Fix archipelago /tutorial

--- 01-services/yarp/_systemd/ReverseProxy/Program.cs
+++ 01-services/yarp/_systemd/ReverseProxy/Program.cs
@@ -1,8 +1,26 @@
+using Yarp.ReverseProxy.Transforms;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddTransforms(ctx =>
+    {
+        // Archipelago doesn't set strict_slashes. This seems to be the only spot where it matters?
+        if (ctx.Cluster?.ClusterId == "archipelago")
+        {
+            ctx.AddRequestTransform(ctx =>
+            {
+                if (ctx.Path == "/tutorial")
+                {
+                    ctx.Path = "/tutorial/";
+                }
+
+                return ValueTask.CompletedTask;
+            });
+        }
+    });
 
 WebApplication app = builder.Build();
 
